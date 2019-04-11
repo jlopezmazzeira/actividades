04408e3d81bc8a9ad6eaec6f4c5128d83636dacc
jlopezmazzeira <jlopezmazzeira@gmail.com>
2019-04-11 17:34:49 -0400

    WIP

--- backend-server/config/config.js
+++ backend-server/config/config.js
@@ -1,4 +1,4 @@
 module.exports.SEED = 'este-es-un-seed-dificl';
-module.exports.USER = 'jesus.lopez.mazzeira@gmail.com';
-module.exports.PASSWORD = 'jk19198862*';
+module.exports.USER = '**********@gmail.com';
+module.exports.PASSWORD = '*********';
 module.exports.SERVICE = 'Gmail';
